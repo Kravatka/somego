97e789533ccaadb463091c6eddcb11fc16d22772
kravatka <yegorkrava@gmail.com>
2021-02-25 01:10:41 +0200

    add crate user test

---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,20 @@
+package store_test
+
+import (
+	"github.com/Kravatka/somego/internal/app/model"
+	"github.com/Kravatka/somego/internal/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
