75234e3cbfc920e633c9678b82d93713b3827674
kravatka <yegorkrava@gmail.com>
2021-02-28 19:48:49 +0200

    draft verification

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,23 +1,30 @@
 package apiserver
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/Kravatka/somego/internal/app/model"
+	"net/http"
+
 	"github.com/Kravatka/somego/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
+var (
+	errIncorrectEmailOrPassword = errors.New("incorrect email or password")
+)
 type server struct {
 	router *mux.Router
 	logger *logrus.Logger
-	store store.Store
+	store  store.Store
 }
 
-func newServer(store store.Store) *server  {
-	s := &server {
+func newServer(store store.Store) *server {
+	s := &server{
 		router: mux.NewRouter(),
 		logger: logrus.New(),
-		store: store,
+		store:  store,
 	}
 
 	s.configureRouter()
@@ -25,16 +32,73 @@ func newServer(store store.Store) *server  {
 	return s
 }
 
-func (s * server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
-func (s *server) configureRouter()  {
+func (s *server) configureRouter() {
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
+	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
 }
 
-func (s * server) handleUsersCreate() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		
+func (s *server) handleUsersCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u := &model.User{
+			Email: req.Email,
+			Password: req.Password,
+		}
+
+		if err := s.store.User().Create(u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+
+func (s *server) handleSessionsCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u, err := s.store.User().FindByEmail(req.Email)
+		if err != nil || !u.ComparePassword(req.Password) {
+			s.error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
 	}
 }
