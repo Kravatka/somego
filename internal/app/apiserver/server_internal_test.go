7a50cfc38207d183749fe8f17d4486c74980050a
kravatka <yegorkrava@gmail.com>
2021-02-28 18:58:12 +0200

    refactoring

---
+++ internal/app/apiserver/server_internal_test.go
@@ -0,0 +1,18 @@
+package apiserver
+
+import (
+	"github.com/Kravatka/somego/internal/app/store/teststore"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServer_HandleUsersCreate(t *testing.T)  {
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+
+	s.ServeHTTP(rec, req)
+	assert.Equal(t, rec.Code, http.StatusOK)
+}
--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -1,51 +1,5 @@
 package store
 
-import (
-	"database/sql"
-
-	_ "github.com/lib/pq" //анонимный импорт что бы методы сами не импортировались в код
-)
-
-type Store struct {
-	config 			*Config
-	db 				*sql.DB
-	userRepository 	*UserRepository
-}
-
-func New(config *Config) *Store {
-	return &Store {
-		config: config,
-	}
-}
-
-func (s *Store) Open() error {
-	db, err := sql.Open("postgres", s.config.DatabaseURL)
-
-	if err != nil {
-		return err
-	}
-
-	s.db = db
-
-	if err := db.Ping(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *Store) Close()  {
-	s.db.Close()
-}
-
-func (s *Store) User() *UserRepository {
-	if s.userRepository != nil {
-		return s.userRepository
-	}
-
-	s.userRepository = &UserRepository {
-		store: s,
-	}
-
-	return s.userRepository
+type Store interface {
+	User() UserRepository
 }
